2935202f305318020a9974dc8056ba8d7fe83fac
Setthapon Thadisakun <steam.reaw1@gmail.com>
2025-02-13 21:17:30 +0700

    update layout and sign in state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { cookies } from 'next/headers';
+import Link from "next/link";
+import {Github} from "lucide-react";
+
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,12 +13,59 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('token')?.value; // undefined if not set
+
+  const userIsSignedIn = Boolean(token);
+
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <NavBar userIsSignedIn={userIsSignedIn} />
+        {children}
+      </body>
     </html>
+  );
+}
+
+function NavBar({ userIsSignedIn }: { userIsSignedIn: boolean }) {
+  return (
+    <header className="px-4 lg:px-6 h-14 flex items-center border-b">
+      <Link className="flex items-center justify-center" href="/">
+        <Github className="h-6 w-6 mr-2"/>
+        <span className="font-bold">TeamUP</span>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="/">
+          Home
+        </Link>
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="/browse">
+          Browse
+        </Link>
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+          My Projects
+        </Link>
+        {userIsSignedIn ? (
+          <>
+            <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+              Profile
+            </Link>
+            <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+              Logout
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link className="text-sm font-medium hover:underline underline-offset-4" href="http://localhost:8080/auth/github">
+              Login
+            </Link>
+          </>
+        )}
+        
+      </nav>
+    </header>
   );
 }
